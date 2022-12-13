e443b9e0bf9140a17fab91930dad12b1ed698a43
Nikita Fursenko <77122703+Nikoscocos@users.noreply.github.com>
2022-12-13 20:02:09 +0400

    Update tilt.js

--- apps/terminal/packages/tilt.js
+++ apps/terminal/packages/tilt.js
@@ -1,3 +1,4 @@
-setWallpaper('apps/terminal/packages/doomer.jpg')
+setWallpaper('apps/terminal/packages/doomer.jpg');
 audio = new Audio('https://str5.pcradio.ru/Nirvana-med');
+audio.volume = 0.3;
 audio.play();
